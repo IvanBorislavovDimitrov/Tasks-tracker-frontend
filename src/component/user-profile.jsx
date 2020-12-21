c77d65e5dd3c374122d998ebe59d072727c62ca0
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2020-12-21 23:33:38 +0200

    Add change password forms and logic

--- src/component/user-profile.jsx
+++ src/component/user-profile.jsx
@@ -1,3 +1,4 @@
+import { configure } from "@testing-library/react";
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 
@@ -25,7 +26,7 @@ class UserProfile extends Component {
                                     <a href="" data-target="#assigned" data-toggle="tab" class="nav-link">Assigned Items</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="" data-target="#edit" data-toggle="tab" class="nav-link">Edit</a>
+                                    <a href="" data-target="#change-password" data-toggle="tab" class="nav-link">Change password</a>
                                 </li>
                             </ul>
                             <div class="tab-content py-4">
@@ -37,7 +38,6 @@ class UserProfile extends Component {
                                             <h5 class="mt-2"><span class="fa fa-clock-o ion-clock float-right"></span>Last 10 logins</h5>
                                             <table class="table table-sm table-hover table-striped">
                                                 <tbody id="last-10-logins">
-
 
                                                 </tbody>
                                             </table>
@@ -52,91 +52,30 @@ class UserProfile extends Component {
                                         </tbody>
                                     </table>
                                 </div>
-                                <div class="tab-pane" id="edit">
+                                <div class="tab-pane" id="change-password">
                                     <form role="form">
-                                        <div class="form-group row">
-                                            <label class="col-lg-3 col-form-label form-control-label">First name</label>
+                                    <div class="form-group row">
+                                            <label class="col-lg-3 col-form-label form-control-label">Current password</label>
                                             <div class="col-lg-9">
-                                                <input class="form-control" type="text" value="Jane" />
+                                                <input id="current-password" class="form-control" type="password" />
                                             </div>
                                         </div>
                                         <div class="form-group row">
-                                            <label class="col-lg-3 col-form-label form-control-label">Last name</label>
+                                            <label class="col-lg-3 col-form-label form-control-label">New Password</label>
                                             <div class="col-lg-9">
-                                                <input class="form-control" type="text" value="Bishop" />
+                                                <input id="new-password" class="form-control" type="password" />
                                             </div>
                                         </div>
                                         <div class="form-group row">
-                                            <label class="col-lg-3 col-form-label form-control-label">Email</label>
+                                            <label class="col-lg-3 col-form-label form-control-label">Confirm new password</label>
                                             <div class="col-lg-9">
-                                                <input class="form-control" type="email" value="email@gmail.com" />
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-lg-3 col-form-label form-control-label">Company</label>
-                                            <div class="col-lg-9">
-                                                <input class="form-control" type="text" value="" />
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-lg-3 col-form-label form-control-label">Website</label>
-                                            <div class="col-lg-9">
-                                                <input class="form-control" type="url" value="" />
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-lg-3 col-form-label form-control-label">Address</label>
-                                            <div class="col-lg-9">
-                                                <input class="form-control" type="text" value="" placeholder="Street" />
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-lg-3 col-form-label form-control-label"></label>
-                                            <div class="col-lg-6">
-                                                <input class="form-control" type="text" value="" placeholder="City" />
-                                            </div>
-                                            <div class="col-lg-3">
-                                                <input class="form-control" type="text" value="" placeholder="State" />
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-lg-3 col-form-label form-control-label">Time Zone</label>
-                                            <div class="col-lg-9">
-                                                <select id="user_time_zone" class="form-control" size="0">
-                                                    <option value="Hawaii">(GMT-10:00) Hawaii</option>
-                                                    <option value="Alaska">(GMT-09:00) Alaska</option>
-                                                    <option value="Pacific Time (US &amp; Canada)">(GMT-08:00) Pacific Time (US &amp; Canada)</option>
-                                                    <option value="Arizona">(GMT-07:00) Arizona</option>
-                                                    <option value="Mountain Time (US &amp; Canada)">(GMT-07:00) Mountain Time (US &amp; Canada)</option>
-                                                    <option value="Central Time (US &amp; Canada)" selected="selected">(GMT-06:00) Central Time (US &amp; Canada)</option>
-                                                    <option value="Eastern Time (US &amp; Canada)">(GMT-05:00) Eastern Time (US &amp; Canada)</option>
-                                                    <option value="Indiana (East)">(GMT-05:00) Indiana (East)</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-lg-3 col-form-label form-control-label">Username</label>
-                                            <div class="col-lg-9">
-                                                <input class="form-control" type="text" value="janeuser" />
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-lg-3 col-form-label form-control-label">Password</label>
-                                            <div class="col-lg-9">
-                                                <input class="form-control" type="password" value="11111122333" />
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-lg-3 col-form-label form-control-label">Confirm password</label>
-                                            <div class="col-lg-9">
-                                                <input class="form-control" type="password" value="11111122333" />
+                                                <input id="confirm-new-password" class="form-control" type="password" />
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label class="col-lg-3 col-form-label form-control-label"></label>
                                             <div class="col-lg-9">
-                                                <input type="reset" class="btn btn-secondary" value="Cancel" />
-                                                <input type="button" class="btn btn-primary" value="Save Changes" />
+                                                <input onClick={this.updatePassword} type="button" class="btn btn-primary" value="Save Changes" />
                                             </div>
                                         </div>
                                     </form>
@@ -146,7 +85,6 @@ class UserProfile extends Component {
                         <div class="col-lg-4 order-lg-1 text-center">
                             <div id="profilePictureSection">
                             </div>
-                            <h6 class="mt-2">Upload a different photo</h6>
                             <br />
                             <div class="form-group">
                                 <label for="pictureInput">Change profile picture</label>
@@ -249,6 +187,45 @@ class UserProfile extends Component {
             alert("The profile picture was updated!");
             window.location.href = '/users/profile';
         }).catch(error => alert(error));
+    }
+
+    updatePassword = () => {
+        const currentPassword = document.getElementById('current-password').value;
+        const newPassword = document.getElementById('new-password').value;
+        const confirmNewPassword = document.getElementById('confirm-new-password').value;
+        if (currentPassword == '' || newPassword == '' || confirmNewPassword == '') {
+            alert("All fields are required!");
+            return;
+        }
+        if (newPassword !== confirmNewPassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+        const updatePasswordRequestModel = {
+            currentPassword: currentPassword,
+            newPassword: newPassword,
+            confirmNewPassword: confirmNewPassword
+        }
+        fetch(process.env.REACT_APP_URL + '/users/update/password', {
+            method: 'PATCH',
+            body: JSON.stringify(updatePasswordRequestModel),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'bearer ' + localStorage.getItem('token')
+            }
+        }).then(async response => {
+            await response.json();
+            if (response.status == 403) {
+                alert('Invalid current password!');
+                return;
+            }
+            if (response.status !== 200) {
+                alert('Error: password has not been updated!');
+                return;
+            }
+            alert('Password has been updated!');
+            window.location.href = '/users/profile';
+        })
     }
 
 }
